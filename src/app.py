9ed75561771059c6e9e81f771d010afdd62e1cb6
jens <jensjorg94@gmail.com>
2022-05-19 15:04:06 +0200

    + init

---
+++ src/app.py
@@ -0,0 +1,25 @@
+# app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def parse(message):
+    pass
+
+
+@app.post("/upload")
+def receive_data():
+    if request.is_json:
+        message = request.get_json()
+        try:
+            parse(message)
+            return 200
+        except Exception as e:
+            print(e)
+            return 400
+    return 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
