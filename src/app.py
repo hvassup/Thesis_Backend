e2975b8add3a1679b562036e7359a356f598123c
jens <jensjorg94@gmail.com>
2022-05-19 15:23:38 +0200

    update post

--- src/app.py
+++ src/app.py
@@ -7,21 +7,17 @@ app = Flask(__name__)
 def parse(message):
     pass
 
-@app.route("/")
-def get_test():
-    return 200
-
 @app.route("/upload")
 def receive_data():
     if request.is_json:
         message = request.get_json()
         try:
             parse(message)
-            return 200
+            return None, 200
         except Exception as e:
             print(e)
-            return 400
-    return 400
+            return None, 400
+    return None, 400
 
 
 if __name__ == '__main__':
